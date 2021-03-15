008d304b69ca802bd823bb6036a590709efeb550
unknown <julzllanillo@gmail.com>
2021-03-15 19:31:40 +0800

    Added resizable components. Added error handlers. Added MD editor

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,29 +1,46 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import CodeEditor from '../components/code-editor';
 import Preview from '../components/preview';
 import bundle from '../bundler';
+import Resizable from './resizable';
 
 const CodeCell = () => {
     const [code, setCode] = useState('');
+    const [err, setErr] = useState('');
     const [input, setInput] = useState('');
 
-    const onClick = async () => {
-        const output = await bundle(input);
-        setCode(output);
-    };
+    useEffect(() => {
+        const timer = setTimeout(async () => {
+            const output = await bundle(input);
+            setCode(output.code);
+            setErr(output.err);
+        }, 1000);
+
+        return () => {
+            // clear running time when user inputs code
+            clearTimeout(timer);
+        };
+    }, [input]);
 
     return (
-        <div>
-            <CodeEditor
-                initialValue="console.log('hello');"
-                onChange={(value) => setInput(value)}
-            />
-            <div>
-                <button onClick={onClick}>Submit</button>
+        <Resizable direction="vertical">
+            <div
+                style={{
+                    height: '100%',
+                    display: 'flex',
+                    flexDirection: 'row',
+                }}
+            >
+                <Resizable direction="horizontal">
+                    <CodeEditor
+                        initialValue="console.log('hello');"
+                        onChange={(value) => setInput(value)}
+                    />
+                </Resizable>
+                <Preview code={code} bundlingStatus={err} />
             </div>
-            <Preview code={code} />
-        </div>
+        </Resizable>
     );
 };
 
